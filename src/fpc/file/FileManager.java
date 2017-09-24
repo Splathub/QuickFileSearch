612ca70738a3ba462b510cf430b9a9c1fe33b24f
CC <holyg@DESKTOP-BM1PPO9>
2017-09-24 15:30:37 -0400

    Save Work CKP

---
+++ src/fpc/file/FileManager.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fpc.file;
+
+import java.nio.file.Path;
+
+/**
+ *
+ * @author Christian Coughlin
+ */
+public class FileManager {
+    private final Path[] divs;
+    int position;
+    
+    
+    
+    FileManager(){
+        //load in save data(~encr)
+        
+        //temp
+               
+        divs = new Path[2];
+    }
+    
+    
+    public void dirSearch(int num){
+
+        for(int i=0; i <= divs.length; i++){
+            //Cd divs[i];
+            
+        }
+    }
+    
+    
+    
+    
+}
